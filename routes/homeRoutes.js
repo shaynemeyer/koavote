9139d50da230d52a0aa8188dfb22f50274d4b322
Shayne Meyer <shayne.meyer@gmail.com>
2016-10-13 23:49:30 -0700

    added mongo cleanup stuff

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,5 +1,8 @@
 var render = require('../lib/render');
+var db = require('./../lib/db');
 
 module.exports.showHome = function *showHome(id) {
-	this.body = yield render("home");
+	var questionLists = yield db.questions.find({});
+
+	this.body = yield render("home", { questions: questionLists });
 };
--- test/home.spec.js
+++ test/home.spec.js
@@ -1,12 +1,45 @@
 var app = require('../app');
 var request = require('supertest').agent(app.listen());
 
+var db = require('./../lib/db');
+var co = require('co');
+var should = require('should');
+var testHelpers = require('./testHelpers');
+
+
 describe("The homepage", function () {
+
+	beforeEach(function (done) {
+		testHelpers.removeAllDocs();
+		done();
+	});
+
+	afterEach(function (done) {
+		testHelpers.removeAllDocs();
+		done();
+	});
+
 	it("displays nicely without errors", function (done) {
 		request
 			.get('/')
 			.expect(200)
 			.expect('Content-type', /html/)
 			.end(done)
+	});
+
+	it("lists all the questions in the database", function (done) {
+		co(function *() {
+			yield db.questions.insert({title: "Question Q1"});
+			yield db.questions.insert({title: "Question Q2"});
+
+			request
+				.get("/")
+				.expect(200)
+				.expect(function (res) {
+					res.text.should.containEql("Question Q1");
+					res.text.should.containEql("Question Q2");
+				})
+				.end(done)
+		})
 	});
 });
